40714856d959b3a855e4898a3e7e6e72c03af9da
sungluil <josungluil@naver.com>
2021-07-03 00:11:39 +0900

    stepContribution, chunkContext 사용해서 Hello world

--- src/main/java/io/spring/batch/HelloWorldApplication.java
+++ src/main/java/io/spring/batch/HelloWorldApplication.java
@@ -2,12 +2,9 @@ package io.spring.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,25 +23,29 @@ public class HelloWorldApplication {
     }
 
     @Bean
-    public Step step() {
-        return this.stepBuilderFactory.get("step")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello world");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
-    }
-
-    @Bean
     public Job job() {
+        System.out.println("job 실행됨");
         return this.jobBuilderFactory.get("job")
-                .start(step())
+                .start(step1())
                 .build();
     }
 
+    @Bean
+    public Step step1() {
+        System.out.println("step1 실행됨");
+        return this.stepBuilderFactory.get("step1")
+
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    System.out.println("Hello world");
+                    return RepeatStatus.FINISHED;
+
+                }).build();
+    }
+
+
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.default", "local");
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
